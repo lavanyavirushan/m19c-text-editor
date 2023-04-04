eb6adde169c40219cb32cf751f73006545d6a738
Lavanya Virushan <lavanya.virushan@gmail.com>
2023-04-04 10:11:56 -0400

    fixing heroku

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static('../client/dist'));
+console.log(__dirname)
+app.use(express.static(__dirname/+'../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
